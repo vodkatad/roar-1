54c39676d6579c046c117a1bf603e3186070e7dc
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-10-13 21:46:02 +0000

    Creating branch for BioC 3.0 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_0/madman/Rpacks/roar@95441 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/Roar-utils.R
@@ -0,0 +1,45 @@
+getFisher <- function(counts) {
+   # treatment_pre treatment_post control_pre control_post
+   mat <- matrix(counts, ncol=2, byrow=FALSE)
+   # Reminders:
+   #colnames(mat) <- c('treatment', 'control')
+   #rownames(mat) <- c('PRE', 'POST')
+   f <- fisher.test(as.table(mat), alternative="two.sided")
+   return(f$p.value)
+}
+
+combineFisherMethod <- function(pvals) {
+   # Computes the combined pvalue from a list of independent tests resulting pvalues
+   # following the fisher method.
+   # http://en.wikipedia.org/wiki/Fisher%27s_method
+   chisq <- -2*sum(log(pvals))
+   return(1-pchisq(chisq, df = 2*length(pvals)))
+}
+
+checkStep <- function(rds, neededStep) {
+   if (rds@step > neededStep) { # Already done, not repeating. Give a reset method?
+      warning("Not repeating a step already done")
+      return(c(FALSE, rds)) # We hope for promotion.
+   } else if (rds@step < neededStep) { # Something is missing.
+      if (neededStep != rds@step) {
+         if (neededStep == 1) {
+            warning("Automatically calling countPrePost")
+            rds <- countPrePost(rds, FALSE)    
+         } else if (neededStep == 2) {
+            warning("Automatically calling computeRoars")
+            rds <- computeRoars(rds)
+         } else if (neededStep == 3) {
+            warning("Automatically calling computePvals")
+            rds <- computePvals(rds)
+         }
+      }
+   } 
+   return(c(TRUE, rds))
+}
+
+meanAcrossAssays <- function(assays, wantedColumns) {
+   # Is the conversion to dataframe slow? Is there a more efficient way without
+   # changing the SE/Assays structure?
+   wantedCols <- lapply(assays, function(x) { x[,wantedColumns] } )
+   return(rowMeans(as.data.frame(wantedCols)))
+}
